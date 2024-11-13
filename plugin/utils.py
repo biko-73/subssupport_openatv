e346c9c3f0697a435d8632d2537a12bb3c578d6f
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-11-14 02:56:06 +0300

    Update utils.py

--- plugin/utils.py
+++ plugin/utils.py
@@ -26,15 +26,13 @@ def load(subpath):
 
 
 def toString(text):
-    if isinstance(text, str):
-        if isinstance(text, six.text_type):
+    if isinstance(text, str) and isinstance(text, six.text_type):
             return six.ensure_str(text)
     return text
 
 
 def toUnicode(text):
-    if isinstance(text, str):
-        if isinstance(text, str):
+    if isinstance(text, str) and isinstance(text, str):
             return six.ensure_text(text, errors='ignore')
     return text
 
